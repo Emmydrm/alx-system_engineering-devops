3af5f51bc37346f5de47307efcc92675b7694f7d
Emmydrm <emmypee222@gmail.com>
2023-02-28 07:19:06 -0800

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-
-input = ARGV[0]
-
-if input =~ /School/
-    puts "The input contains the word 'School'."
-else
-    puts "The input does not contain the word 'School'."
-end
+puts ARGV[0].scan(/school/).join
