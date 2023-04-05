4165310f7583172f5644d693667371a25f4db8ff
Emmydrm <emmypee222@gmail.com>
2023-04-05 02:45:14 -0700

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,10 +1,2 @@
 #!/usr/bin/env ruby
-
-regex = /School/
-input_str = ARGV[0]
-
-if input_str =~ regex
-  puts "The input string '#{input_str}' matches the regex '#{regex}'."
-else
-  puts "The input string '#{input_str}' does not match the regex '#{regex}'."
-end
+puts ARGV[0].scan(/school/i).join
